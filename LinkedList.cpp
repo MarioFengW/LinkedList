f9d05ccb558d0441a0100f9c9a01b06500358ee6
MarioFengW <takatakata195@gmail.com>
2024-10-11 17:31:33 -0600

    Implemented more functions

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -7,47 +7,116 @@ struct Node {
     Node* next;
 };
 
-void addNode(Node* &head, int value) {
-    Node* newNode = new Node(); // Create a new node in memory
-    newNode -> data = value; // Assign the value to the struct data
-    newNode -> next = nullptr; // The next node is nullptr
+void addNodeFinal(Node* &head, int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = nullptr;
 
-    if(head == NULL) {
-        head = newNode; // If the list is empty, the new node is the head
+    if (head == nullptr) {
+        head = newNode;
     } else {
-
-        Node* temp = head; // Create a temp variable to store the current head
-
-        while(temp -> next != nullptr) {
-
-            temp = temp -> next;
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
         }
-
-        temp -> next = newNode; // Enlazar
+        temp->next = newNode;
     }
+}
+
+void addNodeBeginning(Node* &head, int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+}
+
+void addNodePosition(Node* &head, int value, int position) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    
+    if (position == 0) {
+        addNodeBeginning(head, value);
+        return;
+    }
+    
+    Node* temp = head;
+    for (int i = 0; i < position - 1 && temp != nullptr; i++) {
+        temp = temp->next;
+    }
+    
+    if (temp == nullptr) {
+        cout << "La posicion es mayor que el tamaño de la lista." << endl;
+    } else {
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void deleteNode(Node* &head, int value) {
+    if (head == nullptr) return;
+
+    if (head->data == value) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != nullptr && temp->next->data != value) {
+        temp = temp->next;
+    }
+
+    if (temp->next == nullptr) {
+        cout << "El valor no se encuentra en la lista." << endl;
+        return;
+    }
+
+    Node* nodeToDelete = temp->next;
+    temp->next = temp->next->next;
+    delete nodeToDelete;
+}
+
+bool searchNode(Node* head, int value) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        if (temp->data == value) return true;
+        temp = temp->next;
+    }
+    return false;
 }
 
 void showList(Node* head) {
     Node* temp = head;
-    while(temp != nullptr) {
-        cout << temp -> data << "-> ";
-        temp = temp -> next;
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
     }
+    cout << "nullptr" << endl;
 }
 
-
 int main() {
-    
     Node* list = nullptr;
 
-    
-    addNode(list, 40);
-    addNode(list, 10);
-    addNode(list, 490);
+    addNodeFinal(list, 40);
+    addNodeFinal(list, 10);
+    addNodeBeginning(list, 5);
+    addNodePosition(list, 20, 2);
 
     cout << "Lista de valores: " << endl;
-
     showList(list);
+
+    deleteNode(list, 10);
+    cout << "Lista despues de eliminar 10: " << endl;
+    showList(list);
+
+    int valueToSearch = 40;
+
+    if (searchNode(list, valueToSearch)) {
+        cout << "El valor " << valueToSearch << " esta en la lista? Si" << endl;
+    } else {
+        cout << "El valor " << valueToSearch << " esta en la lista? No" << endl;
+    }
 
     return 0;
 }
